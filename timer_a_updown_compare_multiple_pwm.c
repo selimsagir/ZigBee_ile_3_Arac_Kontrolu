a01bba4275972a15cf5d5b91af73b3cba2743bd8
Selim Sağır <selimsagir61@gmail.com>
2017-06-02 22:42:02 +0300

    second commit

--- timer_a_updown_compare_multiple_pwm.c
+++ timer_a_updown_compare_multiple_pwm.c
@@ -1,60 +1,4 @@
-/*
- * -------------------------------------------
- *    MSP432 DriverLib - v3_10_00_09 
- * -------------------------------------------
- *
- * --COPYRIGHT--,BSD,BSD
- * Copyright (c) 2014, Texas Instruments Incorporated
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * *  Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * *  Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * --/COPYRIGHT--*/
-/*******************************************************************************
- * MSP432 PWM TA1.1-2, Up/Down Mode, DCO SMCLK
- *
- * Description: This program generates two PWM outputs on P2.2,3 using
- * Timer1_A configured for up/down mode. The value in CCR0, 128, defines the
- * PWM period/2 and the values in CCR1 and CCR2 the PWM duty cycles. Using
- * ~1.045MHz SMCLK as TACLK, the timer period is ~233us with a 75% duty cycle
- * on P7.7 and 25% on P7.6.
- * SMCLK = MCLK = TACLK = default DCO 3MHz
- *
- *         MSP432P401
- *      -------------------
- *  /|\|                   |
- *   | |                   |
- *   --|RST                |
- *     |                   |
- *     |         P7.7/TA1.1|--> CCR1 - 75% PWM
- *     |         P7.6/TA1.2|--> CCR2 - 25% PWM
- *
- * Author: Timothy Logan
-*******************************************************************************/
+
 /* DriverLib Includes */
 #include "driverlib.h"
 
@@ -124,9 +68,9 @@ const Timer_A_CompareModeConfig compareConfig_PWM2 =
 const eUSCI_UART_Config uartConfig =
 {
         EUSCI_A_UART_CLOCKSOURCE_SMCLK,          // SMCLK Clock Source
-        78,                                     // BRDIV = 78				/* Sayfa 724'deki tablodan bakarak 115200 baudrate ayarlandi*/
+        78,                                      // BRDIV = 78				
         2,                                       // UCxBRF = 2
-        0x0,                                       // UCxBRS = 0			/*115200 BAUDRATE*/
+        0x0,                                     // UCxBRS = 0			/*115200 BAUDRATE*/
         EUSCI_A_UART_NO_PARITY,                  // No Parity
         EUSCI_A_UART_LSB_FIRST,                  // LSB First
         EUSCI_A_UART_ONE_STOP_BIT,               // One stop bit
@@ -346,7 +290,7 @@ void uart_function(void)
 {
 		/* Selecting P1.2 and P1.3 in UART mode */
     	MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P3,
-          GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
+        GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
 
     	/* Configuring UART Module */
     	MAP_UART_initModule(UART_BASE, &uartConfig);
@@ -397,7 +341,6 @@ int main(void)
     	{
     		control_motor();
     		checkUart=0;
-
     	}
 
     }
